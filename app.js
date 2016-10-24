d1472fbb40d3f2a27230b4a884bda7b720a7b339
tuananhta1993 <tatuananh.fi@gmail.com>
2016-10-24 22:43:47 +0300

    change back to Alpha v1

--- app.js
+++ app.js
@@ -25,9 +25,7 @@
 var express = require('express');
 var path = require('path');
 
-fs = require('fs');
-var favicon = require(fs.readFileSync('./lib/favicon.ico'));
-
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
